fb9a1643d4c2b4bfa195e867ce585ccc94fc6164
Elena C <elena.chornobai@gmail.com>
2018-08-12 14:55:52 +0300

    Added forms,inputs

--- src/Components/App.js
+++ src/Components/App.js
@@ -35,12 +35,21 @@ export default class extends Component {
     }))
   }
 
+  handleExerciseCreate = exercise => {
+    this.setState(({ exercises }) => ({
+      exercises: {
+        ...exercises,
+        exercise
+      }
+    }))
+  }
+
   render() {
     const exercises = this.getExercisesByMuscles(),
       { category, exercise } = this.state
 
     return <Fragment>
-      <Header />
+      <Header muscles={muscles} onExreciseCreate={this.handleExerciseCreate} />
 
       <Exercises
         exercise={exercise}
--- src/Components/Exercises/Dialogs/Create.js
+++ src/Components/Exercises/Dialogs/Create.js
@@ -5,23 +5,66 @@ import {
     DialogActions,
     DialogContent,
     DialogContentText,
-    DialogTitle
+    DialogTitle,
+    TextField,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem
 } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import { withStyles } from '@material-ui/core/styles';
 
-export default class extends Component {
+const styles = theme => ({
+    FormControl: {
+        width: 500
+    }
+})
+
+export default withStyles(styles)(class extends Component {
     state = {
-        open: false
+        open: false,
+        exercise: {
+            title: '',
+            description: '',
+            muscles: ''
+        }
     }
 
     handleToggle = () => {
         this.setState({ open: !this.state.open });
     }
 
+    handleChange = name => ({ target: { value } }) => {
+        this.setState({
+            ...this.state.exercise,
+            [name]: value
+        })
+    }
+
+    handleSubmit = () => {
+        const { exercise } = this.state;
+        this.props.onCreate(
+            {
+                ...exercise,
+                id: exercise.title.toLowerCase().replace(/ /g, '-')
+            }
+        );
+        this.setState({
+            open: false,
+            exercise: {
+                title: '',
+                description: '',
+                muscles: ''
+            }
+        })
+    }
+
 
 
     render() {
-        const { open } = this.state;
+        const { open, exercise: { title, description, muscles } } = this.state,
+            { classes, muscles: categories } = this.props;
         return < Fragment >
             <Button variant="fab" mini onClick={this.handleToggle}>
                 <AddIcon />
@@ -29,20 +72,53 @@ export default class extends Component {
             <Dialog
                 open={open}
                 onClose={this.handleToggle}>
-                <DialogTitle id="form-dialog-title">create an exercise</DialogTitle>
+                <DialogTitle>create an exercise</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
                         please fill out the form below
                     </DialogContentText>
                     <form>
+                        <TextField
+                            label="Title"
+                            className={classes.FormControl}
+                            value={title}
+                            onChange={this.handleChange('title')}
+                            margin="normal"
+                        />
+                        <br />
+                        <FormControl className={classes.FormControl}>
+                            <InputLabel htmlFor="muscles">Muscles</InputLabel>
+                            <Select
+                                value={muscles}
+                                onChange={this.handleChange('muscles')}
+                            >
+                                {categories.map(category =>
+                                    <MenuItem value={category}>{category}</MenuItem>)
+                                }
+                            </Select>
+                        </FormControl>
+                        <br />
+                        <TextField
+                            multiline
+                            rows="4"
+                            label="Description"
+                            value={description}
+                            onChange={this.handleChange('description')}
+                            margin="normal"
+                            className={classes.FormControl}
+                        />
                     </form>
                 </DialogContent>
                 <DialogActions>
-                    <Button color="primary" variant="raised">
+                    <Button
+                        color="primary"
+                        variant="raised"
+                        onClick={this.handleSubmit}
+                    >
                         Create
                     </Button>
                 </DialogActions>
             </Dialog>
         </Fragment >
     }
-}
+})
